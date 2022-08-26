08da15d0c0337a102db023e239893c8fe15289b2
hello <hello>
2022-08-26 17:40:48 +0900

    Initial Commit

---
+++ Baemin_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baemin_Assignment
+//
+//  Created by Mac Pro 15 on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
