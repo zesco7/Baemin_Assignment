02b02d64d55808996d5376ee2c56fc513971940c
hello <hello>
2022-08-28 15:00:26 +0900

    -.  6회차 과제 완료: 네비게이션 컨트롤러, 탭바 컨트롤러 연결 및 UI구현, 화면전환 스타일별 생명주기 체크 -. 학습포인트 주석추가: 네비게이션 컨트롤러, 탭바 컨트롤러, 생명주기

--- Baemin_Assignment/ViewController.swift
+++ Baemin_Assignment/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+/*생명주기
+ -. rootview -> childview : 생명주기 print 찍힘(push일때는 찍히나, modal일때는 안찍힘)
+ -. childview -> childview : 생명주기 print 안 찍힘
+ */
+
+/*네비게이션 컨트롤러, 탭바 컨트롤러
+ -. 탭바 6개 이상이면 맨 오른쪽에 more item생겨서 item 누르면 목록으로 표시해줌
+ -. 네비게이션 아이템에서 back button은 해당 페이지가 아닌 이전 페이지의 타이틀로 표시됨(바꾸려면 back button 이름 바꾸면 됨)
+*/
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("hello1")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print("hello2")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("hello3")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("hello4")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("hello5")
     }
 
 
